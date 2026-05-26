5be46ebc833d4dc912bcb4da913c48fe05a92842
Ayoub Taouarda <ayoubtrdone@gmail.com>
2026-05-26 11:16:16 +0000

    Revert "fix: run analytics only in browser"
    This reverts commit 07f4ea513f754a75473f21386ee2b393fbd2f52a.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import posthog from 'posthog-js';
+	import { browser } from '$app/environment';
 
 	import '../app.css';
 	import Header from '../components/shared/Header/Header.svelte';
@@ -8,16 +9,21 @@
 	import { expoIn, expoOut } from 'svelte/easing';
 	import { page } from '$app/stores';
 
+	export const load = async () => {
+		if (browser) {
+			posthog.init('phc_Jzsfs46Oiqa6FB3LFvTMmztSu40Me0tJPWhP6q0AkIe', {
+				api_host: 'https://us.i.posthog.com',
+				defaults: '2025-05-24',
+				person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
+			});
+		}
+	};
+
+	posthog.capture('my event', { property: 'value' });
+
 	let reduceMotion = false;
 
 	onMount(() => {
-		posthog.init('phc_Jzsfs46Oiqa6FB3LFvTMmztSu40Me0tJPWhP6q0AkIe', {
-			api_host: 'https://us.i.posthog.com',
-			defaults: '2025-05-24',
-			person_profiles: 'identified_only' // or 'always' to create profiles for anonymous users as well
-		});
-		posthog.capture('my event', { property: 'value' });
-
 		const media = window.matchMedia('(prefers-reduced-motion: reduce)');
 		const update = () => {
 			reduceMotion = media.matches;
